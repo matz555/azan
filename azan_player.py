fe6c1392b7be08393d68f516d50bda72f804b1c2
matz555 <mkhairi.saad@gmail.com>
2025-01-02 23:20:38 +0800

    Update azan_player.py

--- azan_player.py
+++ azan_player.py
@@ -4,16 +4,48 @@ import json
 import os
 import random
 import datetime
+import requests
+
+def load_zone_from_file(filepath):
+    try:
+        with open(filepath, "r") as file:
+            zone = file.read().strip()
+            return zone
+    except FileNotFoundError:
+        print("Fail zon tidak ditemui. Sila pastikan fail wujud.")
+        return None
+
+def download_prayer_times():
+    zone_file = "/home/pi/backup_azan/zone.txt"  # Lokasi fail zon
+    zone = load_zone_from_file(zone_file)
+
+    if zone:
+        url = f"https://www.e-solat.gov.my/index.php?r=esolatApi/takwimsolat&period=year&zone={zone}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            year = datetime.datetime.now().year
+            filename = f"/home/pi/azan/waktu_solat_{year}.json"
+            with open(filename, "w") as file:
+                file.write(response.text)
+            print(f"Waktu solat untuk zon {zone} tahun {year} disimpan sebagai {filename}")
+        else:
+            print("Gagal memuat turun data waktu solat.")
+    else:
+        print("Zon waktu solat tidak sah. Program dihentikan.")
 
 def load_prayer_times():
     try:
         year = datetime.datetime.now().year
         file_path = f"/home/pi/azan/waktu_solat_{year}.json"
+        if not os.path.exists(file_path):
+            print("Fail JSON waktu solat tidak dijumpai. Memuat turun fail...")
+            download_prayer_times()
+        
         with open(file_path, "r") as file:
             data = json.load(file)
         return data
     except FileNotFoundError:
-        print("Error: Prayer times JSON file not found.")
+        print("Error: Prayer times JSON file not found selepas percubaan memuat turun.")
         return None
     except json.JSONDecodeError:
         print("Error: JSON file is not formatted correctly.")
@@ -105,4 +137,3 @@ def check_and_play_azan():
 
 if __name__ == "__main__":
     check_and_play_azan()
-
